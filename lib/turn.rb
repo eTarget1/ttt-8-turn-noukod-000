be7600978005ccc006a7047cfc4a9affe3b90d30
clifford <clifford@noukod.com>
2018-08-31 18:37:41 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,18 @@
 
-def display_board
-  
-  
+ef display_board(
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts 'display board'
+#board output
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # def turn
 # puts " please enter a number "
 # user_input = gets.chomp
 # user_input.input_to_index
-# if 
+# if
 # end
