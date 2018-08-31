226da016fbf628f27b6a75b0c35836b81461e359
clifford <clifford@noukod.com>
2018-08-31 18:35:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,12 @@
-def turn
-puts " please enter a number "
-user_input = gets.chomp
-user_input.input_to_index
+
+def display_board
+  
+  
 end
+
+# def turn
+# puts " please enter a number "
+# user_input = gets.chomp
+# user_input.input_to_index
+# if 
+# end
